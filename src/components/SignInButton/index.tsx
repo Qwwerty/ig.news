67889649ab6c0f1ef43e7547169d6ab57d324e82
qwwerty <verdinhoraro2009@hotmail.com>
2021-09-20 15:02:35 -0300

    Autenticação com Next Auth

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,30 @@
+import { signIn, signOut, useSession } from 'next-auth/client'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+import styles from './styles.module.scss'
+
+export function SignInButton () {
+  const [session] = useSession()
+
+  console.log(session)
+
+  return session ? (
+    <button
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04D361" />
+      { session.user.name }
+      <FiX color="#737380" />
+    </button>
+  ) : (
+    <button
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
+      <FaGithub color="#EBA417" />
+      Entrar com github
+    </button>
+  )
+}
---
+++ src/components/SubscriptionButton/index.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss'
+
+export function SubscriptionButton () {
+  return (
+    <button
+      type="button"
+      className={styles.subscriptionButton}
+    >
+      Inscrever-se
+    </button>
+  )
+}
