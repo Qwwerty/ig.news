f32481b9f6e4d66918c0d78d52396118ac86c62c
qwwerty <verdinhoraro2009@hotmail.com>
2021-09-21 13:47:27 -0300

    Redirecionando para o Stripe

--- src/components/SubscriptionButton/index.tsx
+++ src/components/SubscriptionButton/index.tsx
@@ -1,16 +1,27 @@
 import styles from './styles.module.scss'
 import {signIn, useSession} from "next-auth/client";
+import {getStripeJS} from "../../services/stripe-js";
+import {api} from "../../services/api";
 
 export function SubscriptionButton () {
   const [session] = useSession()
 
-  function handleSubscription () {
+  async function handleSubscription () {
     if (!session) {
       signIn('github')
       return
     }
 
+    try {
+      const response = await api.post('subscription')
+      const { sessionId } = response.data
 
+      const stripe = await getStripeJS()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (e) {
+      alert(e.message)
+    }
   }
 
   return (
--- src/pages/api/subscription.ts
+++ src/pages/api/subscription.ts
@@ -5,26 +5,30 @@ import { getSession } from 'next-auth/client'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
-    const session = getSession({ req })
+    try {
+      const session = await getSession({ req })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email
-    })
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email
+      })
 
-    const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
-      payment_method_types: ['card'],
-      billing_address_collection: 'required',
-      line_items: [
-        { price: 'price_1JbYhtCramiwGkN3ZtpfNiMD', quantity: 1 }
-      ],
-      mode: 'subscription',
-      allow_promotion_codes: true,
-      success_url: process.env.STRIPE_SUCCESS_URL,
-      cancel_url: process.env.STRIPE_CANCEL_URL
-    })
+      const stripeCheckoutSession = await stripe.checkout.sessions.create({
+        customer: stripeCustomer.id,
+        payment_method_types: ['card'],
+        billing_address_collection: 'required',
+        line_items: [
+          { price: 'price_1JbYhtCramiwGkN3ZtpfNiMD', quantity: 1 }
+        ],
+        mode: 'subscription',
+        allow_promotion_codes: true,
+        success_url: process.env.STRIPE_SUCCESS_URL,
+        cancel_url: process.env.STRIPE_CANCEL_URL
+      })
 
-    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+      return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+    } catch (e) {
+      console.log(e)
+    }
   }
   else {
     res.setHeader('Allow', 'POST')
