10ab1e887c3a7ae7fd0fae0e69c4bb9214e0029e
qwwerty <verdinhoraro2009@hotmail.com>
2021-10-03 20:35:05 -0300

    Página: Post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import styles from './styles.module.scss'
+import Link from "next/link";
 import {GetStaticProps} from "next";
+
+import styles from './styles.module.scss'
+
 import {getPrismicClient} from "../../services/prismic";
 import Prismic from "@prismicio/client";
 import { RichText } from 'prismic-dom'
@@ -26,11 +29,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={`${styles.container} animate__animated animate__backInUp`}>
         <div className={styles.posts}>
           { posts.map(post => (
-            <a href="#" key={post.slug}>
-              <time>{ post.updatedAt }</time>
-              <strong>{ post.title }</strong>
-              <p>{ post.execerpt }</p>
-            </a>
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>{ post.updatedAt }</time>
+                <strong>{ post.title }</strong>
+                <p>{ post.execerpt }</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
@@ -50,7 +55,7 @@ export const getStaticProps: GetStaticProps = async () => {
 
   const posts = response.results.map(post => {
     return {
-      slug: post.id,
+      slug: post.uid,
       title: RichText.asText(post.data.title),
       execerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
       updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
